9a6160c3ed216e696f2d7c69eb78d0d308bdd1b8
Hemanth <gollapudi.hemanth2005@gmail.com>
2025-05-01 19:09:48 +0530

    changes

--- test/test_front.py
+++ test/test_front.py
@@ -4,4 +4,4 @@ def test_frontend_to_backend():
 
     response = requests.get('http://localhost:9000/api/message')
     assert response.status_code == 200
-    assert response.json()['message'] == "Hello from hejhjhjhmanth!"
+    assert response.json()['message'] == "Hello from hemanth!"
