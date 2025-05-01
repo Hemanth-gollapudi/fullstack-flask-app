0dc68c03a8e467dc8e9707fbd02703ed71a6e519
Hemanth <gollapudi.hemanth2005@gmail.com>
2025-05-01 19:08:19 +0530

    chnages

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 document.getElementById('fetchBtn').addEventListener('click', async () => {
-    // const res = await fetch('http://localhost:9000/api/message');   // Changed to 9000
-    const res = await fetch('http://54.161.90.16:9000/api/message');
+    const res = await fetch('http://localhost:9000/api/message');   // Changed to 9000
+    // const res = await fetch('http://54.161.90.16:9000/api/message');
     const data = await res.json();
     document.getElementById('message').innerText = data.message;
 });
