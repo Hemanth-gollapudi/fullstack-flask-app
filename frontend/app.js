9f78bab6aac4617a03606997d8b520e4e21014f1
Hemanth <gollapudi.hemanth2005@gmail.com>
2025-05-02 20:28:23 +0530

    changes done

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchBtn').addEventListener('click', async () => {
-    const res = await fetch('http://backend:9000/api/message');   // Use 'backend' container name
+    const res = await fetch('http://backend:9000/api/message');
     const data = await res.json();
     document.getElementById('message').innerText = data.message;
 });
